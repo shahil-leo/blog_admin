659f4624f89aba63280cfba6751139eeaa83b842
shahil <shahilcode@gmail.com>
2023-03-07 05:07:33 +0530

    connected our firebase to the admin panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,9 +20,9 @@ import { environment } from 'src/environments/environment.development';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // this is the initialisation of connecting the app using the angularFireModule we are connecting the app using the angular
+    // this is the initialization of connecting the app using the angularFireModule we are connecting the app using the angular
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    // this will contain all the modules in the firestore we want to use on by one for any operations related to firebase so we are importing the entire module inside the app so no need to import every single time
+    // this will contain all the modules in the firestore we want to use on by one for any operations related to firebase so we are importing the entire module inside the app so no need to import every single time we can initilize our firestore features on by one using the module imported in the app module
     AngularFirestoreModule,
   ],
   providers: [],
