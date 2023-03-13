986cf8a8edb0dca19de6e7500a0cb102c4683877
shahil <shahilcode@gmail.com>
2023-03-13 15:11:07 +0530

    stored the admin login user and print the email address in the navbar

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  loggedUser!: string
+  localStorage!: any
+  ngOnInit(): void {
+    // we are getting all the details from the local storage from the login user
+    //  And it will be in a string format so we want to convert into object so we can manipulate and get the login user details using that
+    this.localStorage = localStorage.getItem('user')
+    // converting string into json format
+    this.loggedUser = JSON.parse(this.localStorage).email
+  }
 
   isOpen: boolean = false
 
@@ -14,5 +22,4 @@ export class HeaderComponent {
     this.isOpen = !this.isOpen
     console.log(this.isOpen)
   }
-
 }
