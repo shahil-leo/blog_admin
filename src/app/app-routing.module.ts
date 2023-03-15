d3de1c57ea98fa371d6b96803213648fa62cc496
shahil <shahilcode@gmail.com>
2023-03-15 16:00:03 +0530

    added the guard for the project to protect everyone from accessing anywhere

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,12 @@ import { AuthGuard } from './services/auth.guard';
 import { SubscribersComponent } from './subscribers/subscribers.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, },
-  { path: 'categories', component: CategoriesComponent, },
-  { path: 'posts', component: AllPostComponent, },
-  { path: 'posts/new', component: NewPostComponent, },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'categories', component: CategoriesComponent, canActivate: [AuthGuard] },
+  { path: 'posts', component: AllPostComponent, canActivate: [AuthGuard] },
+  { path: 'posts/new', component: NewPostComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'subscribers', component: SubscribersComponent, }
+  { path: 'subscribers', component: SubscribersComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
